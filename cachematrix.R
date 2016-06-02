93a123602778d7cffa05d006dbb1fe239007aeb3
Joseph Fann <josephfann@gmail.com>
2016-06-01 22:31:37 -0400

    CompletedAssignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is programming assignment 2
 
-## Write a short comment describing this function
+## this funciton create a special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(solve) m <<- solve
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## This function check if m exist and return cached m or calculate new. 
 
 cacheSolve <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(x)
+        x$setmean(m)
+        m
         ## Return a matrix that is the inverse of 'x'
 }
